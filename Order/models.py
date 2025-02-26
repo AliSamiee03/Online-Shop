165a6402d8a0217f8b5a5310103ca35700cf7d00
Ali Samiee <ali.samiee.developer@gmail.com>
2025-02-26 12:23:03 +0330

    The order model was added to the order application.

--- Order/models.py
+++ Order/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from Product.models import Product
+from Account.models import CustomUser
 
-# Create your models here.
+class Order(models.Model):
+    STATUS_CHOICE = [
+        ('preparing', 'Preparing'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+        ('pending_payment', 'Pending Payment'),
+    ]
+
+
+    user = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='orders')
+    item = models.ManyToManyField('Product')
+    paid = models.BooleanField(default=False)
+    status = models.CharField(choices=STATUS_CHOICE)
+    final_price = models.PositiveBigIntegerField()
+    discount = models.ForeignKey('Discount', on_delete=models.SET_DEFAULT, default=0)
+
+
+
