398254cd7ba31895057f2e13713477a5cd045c34
Ali Samiee <ali.samiee.developer@gmail.com>
2025-03-18 17:20:42 +0330

    Serializer created for address model

--- Account/serializers.py
+++ Account/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import CustomUser, Address
 
 class UserSerialzer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=20, write_only=True)
@@ -9,3 +9,7 @@ class UserSerialzer(serializers.ModelSerializer):
         fields = ['first_name', 'last_name', 'phone', 'email', 'username', 'password']
 
 
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
