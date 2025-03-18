d32d1708e10e79a7b771b4ebae39f0845039ac54
Ali Samiee <ali.samiee.developer@gmail.com>
2025-03-18 17:29:06 +0330

    Serializer created for Staff model

--- Account/serializers.py
+++ Account/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser, Address
+from .models import CustomUser, Address, Staff, Role
 
 class UserSerialzer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=20, write_only=True)
@@ -13,3 +13,9 @@ class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = '__all__'
+
+class StaffSerializer(serialzers.ModelSerializer):
+    class Meta:
+        model= Staff
+        fields = '__all__'
+
